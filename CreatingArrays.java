c99356eabffe189d314e564a297697e0980b28e3
Jeta Krasniqi <jeta.krasniqi7@student.uni-pr.edu>
2025-03-25 18:25:52 +0100

    Update CreatingArrays.java

--- CreatingArrays.java
+++ CreatingArrays.java
@@ -8,13 +8,13 @@ public class CreatingArrays {
 		int gjatesia_e_vargut;
 		// Sigurohemi që përdoruesi të mos fusë një gjatësi 0 apo negative pasi shfaq error 
 		do {
-            System.out.println("Shtyp nje gjatesi te vargut (duhet te jete > 0): ");
-            gjatesia_e_vargut = input.nextInt();
+                System.out.println("Shtyp nje gjatesi te vargut (duhet te jete > 0): ");
+                gjatesia_e_vargut = input.nextInt();
 
-            if (gjatesia_e_vargut < 0) {
-                System.out.println("Gabim! Gjatesia nuk mund te jete 0 apo negative. Ju lutem provoni perseri.");
-            }
-        } while (gjatesia_e_vargut < 0);
+                	if (gjatesia_e_vargut < 0) {
+                	System.out.println("Gabim! Gjatesia nuk mund te jete 0 apo negative. Ju lutem provoni perseri.");
+           		}
+	       } while (gjatesia_e_vargut < 0);
 		
 		// Shtimi i elementeve ne varg
 		int [] varguIm = new int[gjatesia_e_vargut];
